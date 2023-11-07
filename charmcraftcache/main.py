92a8a4fd4c7108a55fa563d06671681bdce9ea64
Carl Csaposs <carl.csaposs@canonical.com>
2023-11-07 13:46:28 +0000

    Run `charmcraft` commands

--- charmcraftcache/main.py
+++ charmcraftcache/main.py
@@ -1,3 +1,8 @@
+import os
+import pathlib
+import shutil
+import subprocess
+
 import typer
 
 app = typer.Typer()
@@ -5,9 +10,37 @@ app = typer.Typer()
 
 @app.command()
 def pack(cache: bool = True):
-    print("pack")
+    if not shutil.which("charmcraft"):
+        raise Exception("charmcraft not installed")
+    # Check for charmcraft pack wrapper (tox `build` environment)
+    try:
+        tox_environments = subprocess.run(
+            ["tox", "list", "--no-desc"],
+            capture_output=True,
+            check=True,
+            encoding="utf-8",
+        ).stdout.split("\n")
+        if "build" in tox_environments:
+            command = ["tox", "run", "-e", "build"]
+        else:
+            command = ["charmcraft", "pack"]
+    except FileNotFoundError:
+        command = ["charmcraft", "pack"]
+    # Pack charm
+    env = os.environ
+    if cache:
+        cache_directory.mkdir(parents=True, exist_ok=True)
+        env["CRAFT_SHARED_CACHE"] = str(cache_directory)
+        # TODO: download wheels
+    # TODO: add status output
+    subprocess.run(command, check=True, env=env)
 
 
 @app.command()
 def clean():
-    print("clean")
+    # TODO: add status output
+    shutil.rmtree(cache_directory)
+    subprocess.run(["charmcraft", "clean"], check=True)
+
+
+cache_directory = pathlib.Path("~/.cache/charmcraftcache/").expanduser()
