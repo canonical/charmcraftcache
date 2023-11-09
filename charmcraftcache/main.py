443bc94b1a934bb2e995a2d21302140dbe936ba2
Carl Csaposs <carl.csaposs@canonical.com>
2023-11-09 08:29:19 +0000

    Add pip & requests

--- charmcraftcache/main.py
+++ charmcraftcache/main.py
@@ -1,8 +1,11 @@
+import json
 import os
 import pathlib
 import shutil
 import subprocess
+import sys
 
+import requests
 import typer
 
 app = typer.Typer()
@@ -10,31 +13,76 @@ app = typer.Typer()
 
 @app.command()
 def pack(cache: bool = True):
-    if not shutil.which("charmcraft"):
-        raise Exception("charmcraft not installed")
-    # Check for charmcraft pack wrapper (tox `build` environment)
-    try:
-        tox_environments = subprocess.run(
-            ["tox", "list", "--no-desc"],
-            capture_output=True,
-            check=True,
-            encoding="utf-8",
-        ).stdout.split("\n")
-        if "build" in tox_environments:
-            command = ["tox", "run", "-e", "build"]
-        else:
-            command = ["charmcraft", "pack"]
-    except FileNotFoundError:
-        command = ["charmcraft", "pack"]
+    cache_directory.mkdir(parents=True, exist_ok=True)
+    report_file = cache_directory / "report.json"
+    subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            "--dry-run",
+            "-r",
+            "requirements.txt",
+            "--ignore-installed",
+            "--report",
+            str(report_file),
+        ],
+        check=True,
+    )
+    with open(report_file, "r") as file:
+        report = json.load(file)
+    dependencies = report["install"]
     # Pack charm
     env = os.environ
     if cache:
         env["CRAFT_SHARED_CACHE"] = str(cache_directory)
-        cache_directory.mkdir(parents=True, exist_ok=True)
-        # TODO: download wheels
+        charmcraft_cache_subdirectory = (
+            cache_directory / "charmcraft-buildd-base-v2.0/BuilddBaseAlias.JAMMY"
+        )
+        charmcraft_cache_subdirectory.mkdir(parents=True, exist_ok=True)
+        response = requests.get(
+            "https://api.github.com/repos/carlcsaposs-canonical/charmcraftcache-hub/releases/latest",
+            headers={
+                "Accept": "application/vnd.github+json",
+                "X-GitHub-Api-Version": "2022-11-28",
+            },
+        )
+        response.raise_for_status()
+        print("foo")
+        for asset in response.json()["assets"]:
+            for dependency in dependencies:
+                if asset["name"].startswith(
+                    f'{dependency["metadata"]["name"]}-{dependency["metadata"]["version"]}-'
+                ):
+                    # Download wheel
+                    response = requests.get(
+                        f'https://api.github.com/repos/carlcsaposs-canonical/charmcraftcache-hub/releases/assets/{asset["id"]}',
+                        headers={
+                            "Accept": "application/octet-stream",
+                            "X-GitHub-Api-Version": "2022-11-28",
+                        },
+                        stream=True,
+                    )
+                    name, parent = (
+                        asset["name"]
+                        .removesuffix(".charmcraftcachehub")
+                        .split(".charmcraftcachehub.")
+                    )
+                    parent = parent.replace("_", "/")
+                    file_path = charmcraft_cache_subdirectory / parent / name
+                    file_path.parent.mkdir(parents=True, exist_ok=True)
+                    with open(file_path, "wb") as file:
+                        for chunk in response.iter_content():
+                            file.write(chunk)
+                    print("downloaded")
+                    break
+    print("packing")
     # TODO: add status output
     try:
-        subprocess.run(command, capture_output=True, check=True, env=env)
+        subprocess.run(["charmcraft", "pack", "-v"], check=True, env=env)
+    except FileNotFoundError:
+        raise Exception("charmcraft not installed")
     except subprocess.CalledProcessError as e:
         raise Exception(e.stderr)
 
