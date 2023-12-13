6b9869417ac8586ef5c9b23fe8cb80ddc4c40188
Carl Csaposs <carl.csaposs@canonical.com>
2023-12-13 11:09:11 +0000

    Remove --no-cache option
    charmcraft might still use a cache, we can't guarantee it won't

--- charmcraftcache/main.py
+++ charmcraftcache/main.py
@@ -12,7 +12,7 @@ app = typer.Typer()
 
 
 @app.command()
-def pack(cache: bool = True):
+def pack():
     cache_directory.mkdir(parents=True, exist_ok=True)
     report_file = cache_directory / "report.json"
     subprocess.run(
@@ -35,49 +35,48 @@ def pack(cache: bool = True):
     dependencies = report["install"]
     # Pack charm
     env = os.environ
-    if cache:
-        env["CRAFT_SHARED_CACHE"] = str(cache_directory)
-        charmcraft_cache_subdirectory = (
-            cache_directory / "charmcraft-buildd-base-v5.0/BuilddBaseAlias.JAMMY"
-        )
-        charmcraft_cache_subdirectory.mkdir(parents=True, exist_ok=True)
-        response = requests.get(
-            "https://api.github.com/repos/carlcsaposs-canonical/charmcraftcache-hub/releases/latest",
-            headers={
-                "Accept": "application/vnd.github+json",
-                "X-GitHub-Api-Version": "2022-11-28",
-            },
-        )
-        response.raise_for_status()
-        print("foo")
-        for asset in response.json()["assets"]:
-            for dependency in dependencies:
-                if asset["name"].startswith(
-                    f'{dependency["metadata"]["name"].replace("-", "_")}-{dependency["metadata"]["version"]}-'
-                ):
-                    # Download wheel
-                    response = requests.get(
-                        f'https://api.github.com/repos/carlcsaposs-canonical/charmcraftcache-hub/releases/assets/{asset["id"]}',
-                        headers={
-                            "Accept": "application/octet-stream",
-                            "X-GitHub-Api-Version": "2022-11-28",
-                        },
-                        stream=True,
-                    )
-                    response.raise_for_status()
-                    name, parent = (
-                        asset["name"]
-                        .removesuffix(".charmcraftcachehub")
-                        .split(".charmcraftcachehub.")
-                    )
-                    parent = parent.replace("_", "/")
-                    file_path = charmcraft_cache_subdirectory / parent / name
-                    file_path.parent.mkdir(parents=True, exist_ok=True)
-                    with open(file_path, "wb") as file:
-                        for chunk in response.iter_content():
-                            file.write(chunk)
-                    print("downloaded")
-                    break
+    env["CRAFT_SHARED_CACHE"] = str(cache_directory)
+    charmcraft_cache_subdirectory = (
+        cache_directory / "charmcraft-buildd-base-v5.0/BuilddBaseAlias.JAMMY"
+    )
+    charmcraft_cache_subdirectory.mkdir(parents=True, exist_ok=True)
+    response = requests.get(
+        "https://api.github.com/repos/carlcsaposs-canonical/charmcraftcache-hub/releases/latest",
+        headers={
+            "Accept": "application/vnd.github+json",
+            "X-GitHub-Api-Version": "2022-11-28",
+        },
+    )
+    response.raise_for_status()
+    print("foo")
+    for asset in response.json()["assets"]:
+        for dependency in dependencies:
+            if asset["name"].startswith(
+                f'{dependency["metadata"]["name"].replace("-", "_")}-{dependency["metadata"]["version"]}-'
+            ):
+                # Download wheel
+                response = requests.get(
+                    f'https://api.github.com/repos/carlcsaposs-canonical/charmcraftcache-hub/releases/assets/{asset["id"]}',
+                    headers={
+                        "Accept": "application/octet-stream",
+                        "X-GitHub-Api-Version": "2022-11-28",
+                    },
+                    stream=True,
+                )
+                response.raise_for_status()
+                name, parent = (
+                    asset["name"]
+                    .removesuffix(".charmcraftcachehub")
+                    .split(".charmcraftcachehub.")
+                )
+                parent = parent.replace("_", "/")
+                file_path = charmcraft_cache_subdirectory / parent / name
+                file_path.parent.mkdir(parents=True, exist_ok=True)
+                with open(file_path, "wb") as file:
+                    for chunk in response.iter_content():
+                        file.write(chunk)
+                print("downloaded")
+                break
     print("packing")
     # TODO: add status output
     try:
