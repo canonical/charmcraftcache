e69ba3ef951afb0ba345555375fa9207c4255bef
Carl Csaposs <carl.csaposs@canonical.com>
2023-12-14 13:46:24 +0000

    Add versioning for cache invalidation & skip download of wheels already downloaded

--- charmcraftcache/main.py
+++ charmcraftcache/main.py
@@ -1,3 +1,4 @@
+import enum
 import importlib.metadata
 import json
 import logging
@@ -72,7 +73,8 @@ def run_charmcraft(command: list[str]):
             f'charmcraft {version or "not"} installed. charmcraft >=2.5.4 required'
         )
     env = os.environ
-    env["CRAFT_SHARED_CACHE"] = str(cache_directory)
+    env["CRAFT_SHARED_CACHE"] = str(charmcraft_cache_subdirectory)
+    charmcraft_cache_subdirectory.mkdir(parents=True, exist_ok=True)
     if state.verbose:
         command.append("-v")
     try:
@@ -88,6 +90,7 @@ def pack(verbose_: Verbose = False):
         # (Therefore, we should only enable verbose—not disable it)
         state.verbose = True
     logger.info("Resolving dependencies")
+    # todo: check if requirements.txt exists
     report_file = cache_directory / "report.json"
     subprocess.run(
         [
@@ -110,10 +113,11 @@ def pack(verbose_: Verbose = False):
     dependencies = report["install"]
     # Pack charm
     # TODO: remove hardcoded path
-    charmcraft_cache_subdirectory = (
-        cache_directory / "charmcraft-buildd-base-v5.0/BuilddBaseAlias.JAMMY"
+    build_base_cache_subdirectory = (
+        charmcraft_cache_subdirectory
+        / "charmcraft-buildd-base-v5.0/BuilddBaseAlias.JAMMY"
     )
-    charmcraft_cache_subdirectory.mkdir(parents=True, exist_ok=True)
+    build_base_cache_subdirectory.mkdir(parents=True, exist_ok=True)
     logger.debug("Getting latest charmcraftcache-hub release via GitHub API")
     response = requests.get(
         "https://api.github.com/repos/carlcsaposs-canonical/charmcraftcache-hub/releases/latest",
@@ -123,38 +127,53 @@ def pack(verbose_: Verbose = False):
         },
     )
     response.raise_for_status()
-    for asset in rich.progress.track(
-        response.json()["assets"],
+    clean_cache_if_version_changed(
+        VersionType.CHARMCRAFTCACHE_HUB, response.json()["name"].split("-")[-1]
+    )
+    for dependency in rich.progress.track(
+        dependencies,
         description="\[charmcraftcache] Downloading wheels",
         console=console,
     ):
-        for dependency in dependencies:
+        dependency_name = dependency["metadata"]["name"]
+        dependency_version = dependency["metadata"]["version"]
+        for asset in response.json()["assets"]:
             if asset["name"].startswith(
-                f'{dependency["metadata"]["name"].replace("-", "_")}-{dependency["metadata"]["version"]}-'
+                f'{dependency_name.replace("-", "_")}-{dependency_version}-'
             ):
-                # Download wheel
-                response = requests.get(
-                    f'https://api.github.com/repos/carlcsaposs-canonical/charmcraftcache-hub/releases/assets/{asset["id"]}',
-                    headers={
-                        "Accept": "application/octet-stream",
-                        "X-GitHub-Api-Version": "2022-11-28",
-                    },
-                    stream=True,
-                )
-                response.raise_for_status()
+                # todo: wrap request, handle rate limit?
+                # todo: add github auth for rate limit?
                 name, parent = (
                     asset["name"]
                     .removesuffix(".charmcraftcachehub")
                     .split(".charmcraftcachehub.")
                 )
                 parent = parent.replace("_", "/")
-                file_path = charmcraft_cache_subdirectory / parent / name
-                file_path.parent.mkdir(parents=True, exist_ok=True)
-                with open(file_path, "wb") as file:
-                    for chunk in response.iter_content():
-                        file.write(chunk)
-                logger.debug(f"Downloaded {name}")
+                file_path = build_base_cache_subdirectory / parent / name
+                if file_path.exists():
+                    logger.debug(f"{name} already downloaded")
+                else:
+                    # Download wheel
+                    response = requests.get(
+                        f'https://api.github.com/repos/carlcsaposs-canonical/charmcraftcache-hub/releases/assets/{asset["id"]}',
+                        headers={
+                            "Accept": "application/octet-stream",
+                            "X-GitHub-Api-Version": "2022-11-28",
+                        },
+                        stream=True,
+                    )
+                    response.raise_for_status()
+                    file_path.parent.mkdir(parents=True, exist_ok=True)
+                    with open(file_path, "wb") as file:
+                        for chunk in response.iter_content():
+                            file.write(chunk)
+                    logger.debug(f"Downloaded {name}")
                 break
+        else:
+            # TODO: improve message
+            logger.warning(
+                f"No cached wheel found for {dependency_name} {dependency_version}"
+            )
     logger.info("Packing charm")
     run_charmcraft(["pack"])
 
@@ -175,6 +194,9 @@ def clean():
     run_charmcraft(["clean"])
 
 
+# todo: add command for adding charm to charmcraftcache-hub
+
+
 @app.callback()
 def main(verbose: Verbose = False):
     if verbose:
@@ -183,21 +205,35 @@ def main(verbose: Verbose = False):
         state.verbose = True
 
 
+class VersionType(str, enum.Enum):
+    """Type of version number that clears cache directory when changed"""
+
+    CHARMCRAFTCACHE = "charmcraftcache"  # This python package
+    CHARMCRAFTCACHE_HUB = (
+        "charmcraftcachehub"  # GitHub repository where wheels are built
+    )
+
+
+def clean_cache_if_version_changed(version_type: VersionType, current_version: str):
+    file = cache_directory / f"{version_type}_version.txt"
+    try:
+        last_version = file.read_text()
+    except FileNotFoundError:
+        pass
+    else:
+        if last_version != current_version:
+            logger.info(
+                f"{version_type} update from {last_version} to {current_version} detected. Cleaning cache"
+            )
+            clean_cache()
+    finally:
+        file.write_text(current_version)
+
+
 cache_directory = pathlib.Path("~/.cache/charmcraftcache/").expanduser()
 cache_directory.mkdir(parents=True, exist_ok=True)
+charmcraft_cache_subdirectory = cache_directory / "charmcraft"
 state = State()
-# Clean cache if `charmcraftcache` was updated
-charmcraftcache_version = importlib.metadata.version("charmcraftcache")
-version_file = cache_directory / "version.txt"
-try:
-    last_version = version_file.read_text()
-except FileNotFoundError:
-    pass
-else:
-    if last_version != charmcraftcache_version:
-        logger.info(
-            f"Update from {last_version} to {charmcraftcache_version} detected. Cleaning cache"
-        )
-        clean_cache()
-finally:
-    version_file.write_text(charmcraftcache_version)
+clean_cache_if_version_changed(
+    VersionType.CHARMCRAFTCACHE, importlib.metadata.version("charmcraftcache")
+)
